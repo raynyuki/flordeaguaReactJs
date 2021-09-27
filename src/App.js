581014927fb28d871fdc93e3111825cd4f5d151c
Agustina Firpo <86528571+raynyuki@users.noreply.github.com>
2021-09-27 11:46:47 -0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 
 import './App.css';
+import NavBar from './components/Header/NavBar';
+import ItemListContainer from './components/Header/ItemListContainer';
+import React from 'react';
+
+/*class App extends React.Component {
+    render(){
+      return(
+            
+            <div className="App">
+              <NavBar />
+              <h1> Flor de Agua - Jardineria Facil </h1>
+            </div>
+      )
+          
+    };
+  }*/
 
 function App() {
   return (
     <div className="App">
-      <h1> Flor de Agua - Jardineria Facil </h1>
+      <NavBar />
+      <h1> Flor de Agua </h1>
+      <ItemListContainer title="Shop"/>
+
     </div>
   );
 }
---
+++ src/components/Header/NavBar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import CartWidget from './CartWidget';
+import './NavBar.css';
+
+const NavBar = () => {
+    return(
+        <nav className="navbar navbar-light bg-light navbar-expand-lg">
+           <div className="container-fluid">
+               <a className="navbar-brand" href="#">Flor de Agua</a>
+               <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">Shop</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link active" href="#">About Us</a>
+                    </li>
+                </ul> 
+                <CartWidget />
+            </div>
+            
+        </nav>
+    );
+};
+
+export default NavBar;
+
